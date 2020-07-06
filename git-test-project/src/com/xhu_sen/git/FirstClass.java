d1921a03630536003293909f77ba9ab03df7d543
西门吹雪 <hw_xs@163.com>
2020-07-06 19:24:11 +0800

    把本地工程提交到本地仓库

---
+++ git-test-project/src/com/xhu_sen/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.xhu_sen.git;
+
+/**
+ * @author caowudi
+ * @create 2020-07-06-19:15
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+
+    public void test(){
+        System.out.println("change");
+    }
+}
